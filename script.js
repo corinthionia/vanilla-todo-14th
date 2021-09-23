978bac146ef0beb5d5690854126651b325923f75
Joohyun Kim <corinthionia@gmail.com>
2021-09-23 17:33:17 +0900

    :sparkles: feat: add render function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const inputText = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+const doneList = document.querySelector('#done-list');
 
-const todoList = [];
-const doneList = [];
+const items = [];
 
 // 입력한 할 일을 리스트에 추가
 const addNewTodo = () => {
@@ -10,9 +11,23 @@ const addNewTodo = () => {
     isDone: false,
   };
 
-  todoList.push(todoObject);
+  items.push(todoObject);
 
   inputText.value = '';
+  render();
 };
 
 addTodoBtn.addEventListener('click', addNewTodo);
+
+// 할 일 목록을 화면에 렌더하는 함수
+const render = () => {
+  const todoListItem = document.createElement('li');
+  items.map((todo) => {
+    todoListItem.textContent = todo.text;
+    if (todo.isDone) {
+      doneList.append(todoListItem);
+    } else {
+      todoList.append(todoListItem);
+    }
+  });
+};
