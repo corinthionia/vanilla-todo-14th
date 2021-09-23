b65b8bdac6359e4a1d32081fd452b4d1f3d8b295
Joohyun Kim <corinthionia@gmail.com>
2021-09-23 19:05:16 +0900

    :sparkles: feat: add toggleTodo function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputText = document.querySelector('#todo-input');
 const todoList = document.querySelector('#todo-list');
 const doneList = document.querySelector('#done-list');
 
-let items = [];
+let items = [{ text: '할일할일', isDone: false }];
 
 // 입력한 할 일을 리스트에 추가
 const addNewTodo = () => {
@@ -26,6 +26,13 @@ const deleteTodo = (e) => {
   render();
 };
 
+// 할 일의 isDone을 토글
+const toggleTodo = (e) => {
+  const todo = items.find((todo) => todo.text === e.target.textContent);
+  todo.isDone = !todo.isDone;
+  render();
+};
+
 // 할 일 목록을 화면에 렌더하는 함수
 const render = () => {
   todoList.innerHTML = 'TO DO';
@@ -34,6 +41,7 @@ const render = () => {
   items.map((todo) => {
     const todoListItem = document.createElement('li');
     todoListItem.textContent = todo.text;
+    todoListItem.onclick = toggleTodo;
 
     // 삭제 버튼 만들어서 붙이기
     const deleteBtn = document.createElement('i');
@@ -48,3 +56,5 @@ const render = () => {
     }
   });
 };
+
+render();
